4f641eaa406fdebe53f93f844f75155a94eca19b
David Crimi <crimid01@gmail.com>
2024-09-03 14:27:36 -0400

    about page complete

--- components/home/about/Stats.tsx
+++ components/home/about/Stats.tsx
@@ -1,98 +1,127 @@
-import React, { useState } from 'react';
-import Image, {StaticImageData} from 'next/image';
-import styles from './stats.module.scss';
-import HTMLImage from '../../../public/stats-images/HTML.png';
-// import CSSImage from '../../../public/stats-images/css-image.png';
-// import JSImage from '../../../public/stats-images/javascript-image.png';
-import ReactImage from '../../../public/stats-images/REACT.png';
-import FlaskImage from '../../../public/stats-images/FLASK.png';
-// import NodeImage from '../../../public/stats-images/nodejs-image.png';
-import PythonImage from '../../../public/stats-images/PYTHON.png';
-import JavaImage from '../../../public/stats-images/JAVA.png';
-import AWSImage from '../../../public/stats-images/AWS.png';
-import DjangoImage from '../../../public/stats-images/DJANGO.png';
+import React, { useState } from 'react'
+import Image, { StaticImageData } from 'next/image'
+import styles from './stats.module.scss'
+import HTMLImage from '../../../public/stats-images/HTML.png'
+import CSSImage from '../../../public/stats-images/CSS.png'
+import JSImage from '../../../public/stats-images/JS.png'
+import ReactImage from '../../../public/stats-images/REACT.png'
+import FlaskImage from '../../../public/stats-images/FLASK.png'
+import PythonImage from '../../../public/stats-images/PYTHON.png'
+import JavaImage from '../../../public/stats-images/JAVA.png'
+import AWSImage from '../../../public/stats-images/AWS.png'
+import DjangoImage from '../../../public/stats-images/DJANGO.png'
+import CImage from '../../../public/stats-images/C.png'
+import NextImage from '../../../public/stats-images/NEXT.png'
+import GitImage from '../../../public/stats-images/GIT.png'
+import JunitImage from '../../../public/stats-images/JUNIT.png'
+import AndroidImage from '../../../public/stats-images/ANDROID.png'
+import MySQLImage from '../../../public/stats-images/MYSQL.png'
+import PostImage from '../../../public/stats-images/POST.png'
+import NeoImage from '../../../public/stats-images/NEO.png'
+import PandaImage from '../../../public/stats-images/PANDA.png'
+import NumpyImage from '../../../public/stats-images/NP.png'
+import MatplotlibImage from '../../../public/stats-images/MP.png'
+import SkImage from '../../../public/stats-images/SK.png'
 
 // Define the Skill type
 interface Skill {
-  name: string;
-  category: string;
-  imageUrl: StaticImageData;
+  name: string
+  category: string
+  imageUrl: StaticImageData
 }
 
-// Dummy data for skills, you can update this based on your actual skills and categories
 const skillsData: Skill[] = [
   { name: 'HTML', category: 'Front-End', imageUrl: HTMLImage },
-  // { name: 'CSS', category: 'Front-End', imageUrl: CSSImage },
-  // { name: 'JavaScript', category: 'Front-End', imageUrl: JSImage },
+  { name: 'CSS', category: 'Front-End', imageUrl: CSSImage },
+  { name: 'JavaScript', category: 'Front-End', imageUrl: JSImage },
   { name: 'React', category: 'Front-End', imageUrl: ReactImage },
   { name: 'Flask', category: 'Back-End', imageUrl: FlaskImage },
-  // { name: 'Node.js', category: 'Back-End', imageUrl: NodeImage },
   { name: 'Python', category: 'Back-End', imageUrl: PythonImage },
   { name: 'Java', category: 'Back-End', imageUrl: JavaImage },
   { name: 'AWS', category: 'Misc.', imageUrl: AWSImage },
-  { name: 'Django', category: 'Misc.', imageUrl: DjangoImage },
-];
+  { name: 'Django', category: 'Back-End', imageUrl: DjangoImage },
+  { name: 'C', category: 'Back-End', imageUrl: CImage },
+  { name: 'Next,js', category: 'Front-End', imageUrl: NextImage },
+  { name: 'Git', category: 'Misc.', imageUrl: GitImage },
+  { name: 'JUnit', category: 'Misc.', imageUrl: JunitImage },
+  { name: 'Android Studio', category: 'Misc.', imageUrl: AndroidImage },
+  { name: 'MySQL', category: 'Back-End', imageUrl: MySQLImage },
+  { name: 'Postgre SQL', category: 'Back-End', imageUrl: PostImage },
+  { name: 'Neo4j', category: 'Back-End', imageUrl: NeoImage },
+  { name: 'Numpy', category: 'Misc.', imageUrl: NumpyImage },
+  { name: 'Pandas', category: 'Misc.', imageUrl: PandaImage },
+  { name: 'Matplotlib', category: 'Misc.', imageUrl: MatplotlibImage },
+  { name: 'Scikit-learn', category: 'Misc.', imageUrl: SkImage },
+]
 
 const Skills: React.FC = () => {
   const [filters, setFilters] = useState<{ [key: string]: boolean }>({
     'Front-End': false,
     'Back-End': false,
     'Misc.': false,
-  });
+  })
 
   const handleFilterChange = (category: string) => {
-    setFilters((prev) => ({ ...prev, [category]: !prev[category] }));
-  };
+    setFilters((prev) => ({ ...prev, [category]: !prev[category] }))
+  }
 
   const filteredSkills = skillsData.filter(
-      (skill) =>
-          (filters['Front-End'] && skill.category === 'Front-End') ||
-          (filters['Back-End'] && skill.category === 'Back-End') ||
-          (filters['Misc.'] && skill.category === 'Misc.') ||
-          (!filters['Front-End'] && !filters['Back-End'] && !filters['Misc.'])
-  );
+    (skill) =>
+      (filters['Front-End'] && skill.category === 'Front-End') ||
+      (filters['Back-End'] && skill.category === 'Back-End') ||
+      (filters['Misc.'] && skill.category === 'Misc.') ||
+      (!filters['Front-End'] && !filters['Back-End'] && !filters['Misc.'])
+  )
 
   return (
-      <div className={`${styles.skillsSection} flex flex-col items-center justify-start p-6`}>
-        <div className={`${styles.filters} mb-6`}>
-          <label className="flex items-center">
-            <input
-                type="checkbox"
-                checked={filters['Front-End']}
-                onChange={() => handleFilterChange('Front-End')}
-                className="mr-3"
-            />
-            Front-End
-          </label>
-          <label className="flex items-center">
-            <input
-                type="checkbox"
-                checked={filters['Back-End']}
-                onChange={() => handleFilterChange('Back-End')}
-                className="mr-3"
-            />
-            Back-End
-          </label>
-          <label className="flex items-center">
-            <input
-                type="checkbox"
-                checked={filters['Misc.']}
-                onChange={() => handleFilterChange('Misc.')}
-                className="mr-3"
-            />
-            Misc.
-          </label>
-        </div>
-        <div className="skills-container grid grid-cols-3 gap-6 max-h-96 overflow-y-scroll">
-          {filteredSkills.map((skill) => (
-              <div key={skill.name} className={styles.skillCard}>
-                <Image src={skill.imageUrl} alt={skill.name} className={styles.skillImage} width={50} height={50} />
-                <p>{skill.name}</p>
-              </div>
-          ))}
-        </div>
+    <div
+      className={`${styles.skillsSection} w-full flex flex-col items-center justify-start p-6`}
+    >
+      <div className={`${styles.filters} mb-6`}>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            checked={filters['Front-End']}
+            onChange={() => handleFilterChange('Front-End')}
+            className="mr-3"
+          />
+          Front-End
+        </label>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            checked={filters['Back-End']}
+            onChange={() => handleFilterChange('Back-End')}
+            className="mr-3"
+          />
+          Back-End
+        </label>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            checked={filters['Misc.']}
+            onChange={() => handleFilterChange('Misc.')}
+            className="mr-3"
+          />
+          Misc.
+        </label>
       </div>
-  );
-};
+      <div className="skills-container grid grid-cols-2 gap-4 max-h-96 overflow-y-scroll">
+        {filteredSkills.map((skill) => (
+          <div key={skill.name} className={styles.skillCard}>
+            <Image
+              src={skill.imageUrl}
+              alt={skill.name}
+              className={styles.skillImage}
+              width={50}
+              height={50}
+            />
+            <p>{skill.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
-export default Skills;
+export default Skills
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,46 @@
-import Head from "next/head";
-import { Home } from "@/components/home/Home";
+import Head from 'next/head'
+import { Home } from '@/components/home/Home'
 
 export default function home() {
   return (
-      <>
-        <Head>
-          <title>David Crimi | Software Developer</title>
-          <link rel="shortcut icon" href="/favicon.png" />
-          <meta name="description" content="David Crimi | Software Developer" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta property="og:title" content="David Crimi | Software Developer" />
-          <meta name="description" content="Hey! I'm David Crimi." />
+    <>
+      <Head>
+        <title>David Crimi | Software Developer</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta name="description" content="David Crimi | Software Developer" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="David Crimi | Software Developer" />
+        <meta name="description" content="Hey! I'm David Crimi." />
 
-          <meta property="og:title" content="David Crimi | Software Developer" />
-          <meta property="og:type" content="article" />
-          <meta property="og:url" content="https://deverajc.com/" />
-          <meta property="og:image" content="https://deverajc.com/screenshot.png" />
-          <meta property="og:description" content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate." />
+        <meta property="og:title" content="David Crimi | Software Developer" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://deverajc.com/" />
+        <meta
+          property="og:image"
+          content="https://deverajc.com/screenshot.png"
+        />
+        <meta
+          property="og:description"
+          content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate."
+        />
 
-          <meta name="twitter:card" content="summary"/>
-          <meta name="twitter:site" content="@jc_devz"/>
-          <meta name="twitter:title" content="John Carlo Devera | Frontend Developer"/>
-          <meta name="twitter:description" content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate."/>
-          <meta name="twitter:creator" content="@jc_devz"/>
-          <meta name="twitter:image" content="https://deverajc.com/screenshot.png"/>
-
-        </Head>
-        <Home />
-      </>
-  );
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@jc_devz" />
+        <meta
+          name="twitter:title"
+          content="John Carlo Devera | Frontend Developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate."
+        />
+        <meta name="twitter:creator" content="@jc_devz" />
+        <meta
+          name="twitter:image"
+          content="https://deverajc.com/screenshot.png"
+        />
+      </Head>
+      <Home />
+    </>
+  )
 }

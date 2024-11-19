a22a7a6956643ce3a7338198d27ebaecfd1818b3
David Crimi <crimid01@gmail.com>
2024-11-19 10:10:27 -0500

    Updated hero page

--- components/home/hero/DotGrid.tsx
+++ components/home/hero/DotGrid.tsx
@@ -1,98 +1,87 @@
-import React, { useState, useEffect } from "react";
-import styles from "./dotgrid.module.scss";
-import anime from "animejs";
+"use client"
 
-export const DotGrid = () => {
-  const GRID_WIDTH = 25;
-  const GRID_HEIGHT = 20;
+import React, { Suspense, useRef } from "react"
+import { Html, OrbitControls, useGLTF } from "@react-three/drei"
+import { Canvas, useFrame } from "@react-three/fiber"
+import * as THREE from "three"
 
-  const [ripples, setRipples] = useState<{ x: number; y: number; key: number }[]>([]);
-  const [key, setKey] = useState(0);
+function Model() {
+  const group = useRef<any>()
+  const { nodes, materials } = useGLTF(
+      "https://rwxrdxvxndclnqvznxfj.supabase.co/storage/v1/object/public/test/scene-final-test.glb?t=2023-11-29T02%3A03%3A37.877Z"
+  ) as any
 
-  const handleDotClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    const index = e.currentTarget.dataset.index;
-    setRipples((prev) => [
-      ...prev,
-      { x: e.clientX, y: e.clientY, key }
-    ]);
-    setKey((prevKey) => prevKey + 1);
-
-    anime({
-      targets: ".dot-point",
-      scale: [
-        { value: 1.35, easing: "easeOutSine", duration: 250 },
-        { value: 1, easing: "easeInOutQuad", duration: 500 },
-      ],
-      translateY: [
-        { value: -15, easing: "easeOutSine", duration: 250 },
-        { value: 0, easing: "easeInOutQuad", duration: 500 },
-      ],
-      opacity: [
-        { value: 0.7, easing: "easeOutSine", duration: 250 },
-        { value: 1, easing: "easeInOutQuad", duration: 500 },
-      ],
-      backgroundColor: [
-        { value: "#FF6347", easing: "easeOutSine", duration: 250 },
-        { value: "#007BFF", easing: "easeInOutQuad", duration: 500 },
-      ],
-      delay: anime.stagger(100, {
-        grid: [GRID_WIDTH, GRID_HEIGHT],
-        from: index ? parseInt(index) : 0,
-      }),
-    });
-  };
-
-  useEffect(() => {
-    if (ripples.length > 0) {
-      const lastRipple = ripples[ripples.length - 1];
-      anime({
-        targets: `.ripple-${lastRipple.key}`,
-        scale: [0, 4],
-        opacity: [1, 0],
-        duration: 1000,
-        easing: "easeOutQuad",
-        complete: () => {
-          setRipples((prev) => prev.filter((r) => r.key !== lastRipple.key));
-        },
-      });
-    }
-  }, [ripples]);
-
-  const dots = [];
-
-  let index = 0;
-  for (let i = 0; i < GRID_WIDTH; i++) {
-    for (let j = 0; j < GRID_HEIGHT; j++) {
-      dots.push(
-          <div
-              onClick={handleDotClick}
-              className={styles.dotWrapper}
-              data-index={index.toString()}
-              key={`${i}-${j}`}
-          >
-            <div
-                className={`${styles.dot} dot-point ${index}`}
-                data-index={index.toString()}
-            />
-          </div>
-      );
-      index++;
-    }
-  }
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime()
+    group.current.rotation.x = THREE.MathUtils.lerp(
+        group.current.rotation.x,
+        Math.cos(t / 2) / 20 + 0.25,
+        0.1
+    )
+    group.current.rotation.y = THREE.MathUtils.lerp(
+        group.current.rotation.y,
+        Math.sin(t / 4) / 20,
+        0.1
+    )
+    group.current.rotation.z = THREE.MathUtils.lerp(
+        group.current.rotation.z,
+        Math.sin(t / 8) / 20,
+        0.1
+    )
+    group.current.position.y = THREE.MathUtils.lerp(
+        group.current.position.y,
+        (-2 + 0.9 * Math.sin(t / 2)) / 2,
+        0.1
+    )
+  })
 
   return (
-      <div
-          style={{ gridTemplateColumns: `repeat(${GRID_WIDTH}, 1fr)` }}
-          className={`${styles.dotGrid} hidden sm:block`} // Tailwind classes added here
-      >
-        {dots.map((dot) => dot)}
-        {ripples.map((ripple) => (
-            <div
-                key={ripple.key}
-                className={`${styles.ripple} ripple-${ripple.key}`}
-                style={{ left: ripple.x, top: ripple.y }}
+      <group ref={group} dispose={null}>
+        <group rotation-x={-0.225}>
+          <group position={[-0, -3, -0.3]} rotation={[-Math.PI / 2, 0, 0]}>
+            <mesh
+                geometry={nodes.CRTMonitor__0.geometry}
+                material={materials["Scene_-_Root"]}
+                scale={0.3}
             />
-        ))}
+            <Html
+                className="content border-lg h-[195px] w-[250px] overflow-y-auto rounded-sm bg-white p-4"
+                rotation-x={Math.PI / 2.06}
+                position={[0, -3.768, 4.63]}
+                transform
+                occlude
+            >
+              <div className="wrapper overflow-hidden" onPointerDown={(e) => e.stopPropagation()}>
+                <h1 className="font-bold text-black text-center text-xl tracking-tighter md:text-4xl">
+                  This should<br /> be your Heading
+                </h1>
+              </div>
+            </Html>
+          </group>
+        </group>
+      </group>
+  )
+}
+
+export function FloatingPcGrayscale() {
+  return (
+      <div className="h-[400px] w-[400px] cursor-grab">
+        <Canvas camera={{ position: [-10, 0, -20], fov: 55 }}>
+          <pointLight position={[-10, 10, -40]} intensity={10000} />
+          <Suspense fallback={null}>
+            <group rotation={[0, Math.PI, 0]} position={[0, 1, 0]}>
+              <Model />
+            </group>
+          </Suspense>
+          <OrbitControls
+              enablePan={false}
+              enableZoom={false}
+              minPolarAngle={Math.PI / 2.2}
+              maxPolarAngle={Math.PI / 2.2}
+              minAzimuthAngle={Math.PI / 1.1}
+              maxAzimuthAngle={-Math.PI / 1.3}
+          />
+        </Canvas>
       </div>
-  );
-};
+  )
+}
--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -2,52 +2,45 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { StandardButton } from "@/components/buttons/StandardButton";
 import { Reveal } from "@/components/utils/Reveal";
-import { DotGrid } from "./DotGrid";
+import { FloatingPcGrayscale } from "./DotGrid";
 import styles from "./hero.module.scss";
 import Profile from "@/public/portrait.png";
 
 export const Hero = () => {
   return (
-    <section className={`section-wrapper ${styles.hero}`}>
-      <div className={styles.heroGrid}>
-        <div className={styles.copyWrapper}>
-          <Reveal>
-            <h1 className={styles.title}>
-              Hi, I&apos;m David.<span className={styles.circle}></span>
-            </h1>
-          </Reveal>
-          <Reveal>
-            <h2 className={styles.subTitle}>
-              I&apos;m a{' '}
-              <span className={styles.highlight}>Software Developer</span>
-              <span className={styles.circle}></span>
-            </h2>
-          </Reveal>
-          <Reveal>
-            <p className={styles.aboutCopy}>
-              Thank you for dropping by I am thrilled to have you here. I am a
-              fourth year computer science student with a passion for creating
-              and learning new things. Here you&apos;ll learn more about me and
-              some of the cool things I have done.
-            </p>
-          </Reveal>
+      <section className={`section-wrapper ${styles.hero}`}>
+        <div className={styles.heroGrid}>
+          <div className={styles.copyWrapper}>
+            <Reveal>
+              <h1 className={styles.title}>
+                Hi, I&apos;m David.<span className={styles.circle}></span>
+              </h1>
+            </Reveal>
+            <Reveal>
+              <h2 className={styles.subTitle}>
+                I&apos;m a{' '}
+                <span className={styles.highlight}>Software Developer</span>
+                <span className={styles.circle}></span>
+              </h2>
+            </Reveal>
+            <Reveal>
+              <p className={styles.aboutCopy}>
+                Thank you for dropping by I am thrilled to have you here. I am a
+                fourth year computer science student with a passion for creating
+                and learning new things. Here you&apos;ll learn more about me and
+                some of the cool things I have done.
+              </p>
+            </Reveal>
+          </div>
+          <motion.div
+              initial={{ y: -30, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+              className="relative w-[500px] h-[4  00px] hidden md:block"
+          >
+            <FloatingPcGrayscale />
+          </motion.div>
         </div>
-        <motion.div
-          initial={{ y: -30, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.4 }}
-        >
-          <Image
-            className={styles.profile}
-            src={Profile}
-            priority
-            alt="John Carlo Devera | Frontend Developer"
-            width={250}
-            height={250}
-          />
-        </motion.div>
-      </div>
-      <DotGrid />
-    </section>
-  )
+      </section>
+  );
 };
